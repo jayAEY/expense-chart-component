765fc453fc7271568d0ab8d5da542f50c9dddce2
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-05-24 08:03:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let currentDay = (new Date).getDay()
+let bars = document.querySelectorAll('.bar')
+let tooltip = document.querySelector('#tooltip')
+
+function changeHeight(bar, spentInDay) {
+  bar.style.dataSpent = spentInDay
+  if (spentInDay > 55) {
+    bar.style.height = "100%"
+  } else {
+    bar.style.height = `${spentInDay * 1.8}%`
+  }
+}
+
+function handleTooltip(bar) {
+  let barCoords = bar.getBoundingClientRect()
+  tooltip.style.display = "block"
+  tooltip.innerText = `$${bar.style.dataSpent}`
+  tooltip.style.left = `${(barCoords.x - ((tooltip.getBoundingClientRect().width - barCoords.width) / 2))}px`
+  tooltip.style.top = `${(barCoords.top) - 30}px`
+}
+
+async function getData() {
+  let response = await fetch("./data.json");
+  let data = await response.json()
+  for (let i = 0; i < bars.length; i++) {
+    changeHeight(bars[i], data[i].amount)
+  }
+  bars[currentDay].style.backgroundColor = "hsl(186, 34%, 60%)";
+}
+
+window.onload = getData
+bars.forEach(bar => bar.addEventListener("mouseover", () => handleTooltip(bar)))
+bars.forEach(bar => bar.addEventListener("mouseout", () => tooltip.style.display = "none"))
